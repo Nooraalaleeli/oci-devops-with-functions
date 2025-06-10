18f0746f01c72f31abfa472f11c0fa6f961283e7
Nooraalaleeli <nooramusabeh1@gmail.com>
2025-06-11 00:57:29 +0400

    Update datasources.tf

--- datasources.tf
+++ datasources.tf
@@ -2,34 +2,25 @@
 ## All rights reserved. The Universal Permissive License (UPL), Version 1.0 as shown at http://oss.oracle.com/licenses/upl
 # 
 
-# Gets a list of Availability Domains
+# Availability Domains
 data "oci_identity_availability_domains" "ADs" {
   compartment_id = var.tenancy_ocid
 }
 
-# Gets home and current regions
-data "oci_identity_tenancy" "tenant_details" {
-  tenancy_id = var.tenancy_ocid
-
-  provider = oci.current_region
-}
-
-data "oci_identity_regions" "home_region" {
-  filter {
-    name   = "key"
-    values = [data.oci_identity_tenancy.tenant_details.home_region_key]
-  }
-
-  provider = oci.current_region
-}
-
-
+# Tenancy details
 data "oci_identity_tenancy" "oci_tenancy" {
   tenancy_id = var.tenancy_ocid
 }
 
+# Regions (filtered for current region)
+data "oci_identity_regions" "oci_regions" {
+  filter {
+    name   = "name"
+    values = [var.region]
+  }
+}
+
 # OCI Services
-## Available Services
 data "oci_core_services" "all_services" {
   filter {
     name   = "name"
@@ -38,23 +29,19 @@ data "oci_core_services" "all_services" {
   }
 }
 
-data "oci_identity_regions" "oci_regions" {
-
-  filter {
-    name   = "name"
-    values = [var.region]
-  }
-
-}
-
+# Object Storage Namespace — uses tenancy OCID, not compartment
 data "oci_objectstorage_namespace" "os_namespace" {
   compartment_id = var.tenancy_ocid
 }
 
-
-# Randoms
+# Random value generator
 resource "random_string" "deploy_id" {
   length  = 4
   special = false
 }
 
+# Use static locals instead of broken dynamic region lookup
+locals {
+  ocir_docker_repository = "${lower(var.region)}.ocir.io"
+  ocir_namespace         = data.oci_objectstorage_namespace.os_namespace.namespace
+}
