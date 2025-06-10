0ca0cac9df40da9b0867c1fd9e87e9033d0a41ab
Nooraalaleeli <nooramusabeh1@gmail.com>
2025-06-11 01:54:29 +0400

    Update locals.tf

--- locals.tf
+++ locals.tf
@@ -2,17 +2,14 @@
 ## All rights reserved. The Universal Permissive License (UPL), Version 1.0 as shown at http://oss.oracle.com/licenses/upl
 
 locals {
+  app_name_lower = lower(var.app_name)
+
+  allow_devops_manage_compartment_statements = [
+    "Allow dynamic-group your-dg-name-here to manage all-resources in tenancy"
+  ]
+
   devops_compartment_statements = concat(
     local.allow_devops_manage_compartment_statements,
   )
 }
 
-locals {
-  app_name_lower = lower(var.app_name)
-}
-
-locals {
-  allow_devops_manage_compartment_statements = [
-    "Allow dynamic-group your-dg-name-here to manage all-resources in tenancy"
-  ]
-}
