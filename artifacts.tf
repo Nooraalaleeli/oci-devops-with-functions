d1afc6f96395fee76dc15d8fefc8747aa09ddf43
Nooraalaleeli <nooramusabeh1@gmail.com>
2025-06-11 02:02:09 +0400

    Update artifacts.tf

--- artifacts.tf
+++ artifacts.tf
@@ -1,11 +1,25 @@
-resource "oci_artifacts_container_repository" "test_container_repository" {
-  compartment_id = var.compartment_ocid
-  display_name   = "oci-function-python-defaultimage-${random_id.tag.hex}"
-  is_public      = var.container_repository_is_public
+resource "oci_devops_deploy_artifact" "test_deploy_artifact_default" {
+  argument_substitution_mode = var.deploy_artifact_argument_substitution_mode
+  deploy_artifact_source {
+    deploy_artifact_source_type = var.deploy_artifact_deploy_artifact_source_deploy_artifact_source_type
+    image_uri     = "${local.ocir_docker_repository}/${local.ocir_namespace}/${oci_artifacts_container_repository.test_container_repository.display_name}:$${BUILDRUN_HASH}"
+    image_digest  = " "
+    repository_id = oci_devops_repository.test_repository.id
+  }
+  deploy_artifact_type = var.deploy_artifact_deploy_artifact_type
+  project_id           = oci_devops_project.test_project.id
+  display_name         = "artifact-default"
 }
 
-resource "oci_artifacts_container_repository" "test_container_repository2" {
-  compartment_id = var.compartment_ocid
-  display_name   = "oci-function-python-customimage-${random_id.tag.hex}"
-  is_public      = var.container_repository_is_public
+resource "oci_devops_deploy_artifact" "test_deploy_artifact_custom" {
+  argument_substitution_mode = var.deploy_artifact_argument_substitution_mode
+  deploy_artifact_source {
+    deploy_artifact_source_type = var.deploy_artifact_deploy_artifact_source_deploy_artifact_source_type
+    image_uri     = "${local.ocir_docker_repository}/${local.ocir_namespace}/${oci_artifacts_container_repository.test_container_repository2.display_name}:$${BUILDRUN_HASH}"
+    image_digest  = " "
+    repository_id = oci_devops_repository.test_repository.id
+  }
+  deploy_artifact_type = var.deploy_artifact_deploy_artifact_type
+  project_id           = oci_devops_project.test_project.id
+  display_name         = "artifact-custom"
 }
