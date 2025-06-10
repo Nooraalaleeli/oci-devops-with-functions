921ccde6cf8176b7f07f84c022435239d8c6c9ea
Nooraalaleeli <nooramusabeh1@gmail.com>
2025-06-11 01:26:39 +0400

    Update functions-policies.tf

--- functions-policies.tf
+++ functions-policies.tf
@@ -20,8 +20,10 @@ resource "oci_identity_policy" "admin_manage_function_family_policy" {
   name           = "${var.app_name}-admin-manage-function-family-policy-${random_id.tag.hex}"
   description    = "${var.app_name}-admin-manage-function-family-policy-${random_id.tag.hex}"
   compartment_id = var.compartment_ocid
-  statements = ["Allow group Administrators to manage functions-family in compartment id ${var.compartment_ocid}",
-  "Allow group Administrators to read metrics in compartment id ${var.compartment_ocid}"]
+  statements = [
+    "Allow group Administrators to manage functions-family in compartment id ${var.compartment_ocid}",
+    "Allow group Administrators to read metrics in compartment id ${var.compartment_ocid}"
+  ]
   provisioner "local-exec" {
     command = "sleep 5"
   }
@@ -34,7 +36,6 @@ resource "oci_identity_policy" "admin_use_vcn_family_policy" {
   description    = "${var.app_name}-admin-use-vcn-family-policy-${random_id.tag.hex}"
   compartment_id = var.compartment_ocid
   statements     = ["Allow group Administrators to use virtual-network-family in compartment id ${var.compartment_ocid}"]
-
   provisioner "local-exec" {
     command = "sleep 5"
   }
@@ -47,7 +48,6 @@ resource "oci_identity_policy" "faas_use_vcn_family_policy" {
   description    = "${var.app_name}-faas-use-vcn-family-policy-${random_id.tag.hex}"
   compartment_id = var.tenancy_ocid
   statements     = ["Allow service FaaS to use virtual-network-family in compartment id ${var.compartment_ocid}"]
-
   provisioner "local-exec" {
     command = "sleep 5"
   }
@@ -60,7 +60,6 @@ resource "oci_identity_dynamic_group" "faas_dg" {
   description    = "${var.app_name}-faas-dg-${random_id.tag.hex}"
   compartment_id = var.tenancy_ocid
   matching_rule  = "ALL {resource.type = 'fnfunc', resource.compartment.id = '${var.compartment_ocid}'}"
-
   provisioner "local-exec" {
     command = "sleep 5"
   }
@@ -73,7 +72,6 @@ resource "oci_identity_policy" "faas_dg_policy" {
   description    = "${var.app_name}-faas-dg-policy-${random_id.tag.hex}"
   compartment_id = var.compartment_ocid
   statements     = ["allow dynamic-group ${oci_identity_dynamic_group.faas_dg.name} to manage all-resources in compartment id ${var.compartment_ocid}"]
-
   provisioner "local-exec" {
     command = "sleep 5"
   }
