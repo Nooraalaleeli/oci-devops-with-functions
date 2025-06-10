fcb47325425d230d8a377f89eb9d8d165b6362c6
Nooraalaleeli <nooramusabeh1@gmail.com>
2025-06-11 00:59:58 +0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -74,10 +74,6 @@ variable "oci_user_authtoken" {
 }
 
 # OCIR repo name & namespace
-locals {
-  ocir_docker_repository = join("", [lower(lookup(data.oci_identity_regions.oci_regions.regions[0], "key")), ".ocir.io"])
-  ocir_namespace         = lookup(data.oci_objectstorage_namespace.os_namespace, "namespace")
-}
 
 variable "create_dynamic_group_for_nodes_in_compartment" {
   default = true
