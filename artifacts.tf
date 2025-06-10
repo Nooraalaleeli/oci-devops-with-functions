dc8a0e226d690dac5e43f38192be38640dc91c27
Nooraalaleeli <nooramusabeh1@gmail.com>
2025-06-11 02:00:08 +0400

    Update artifacts.tf

--- artifacts.tf
+++ artifacts.tf
@@ -5,8 +5,7 @@ resource "oci_artifacts_container_repository" "test_container_repository" {
 }
 
 resource "oci_artifacts_container_repository" "test_container_repository2" {
-  compartment_id   = var.compartment_ocid
-  display_name     = "oci-function-python-customimage-${random_id.tag.hex}"
-  is_public        = var.container_repository_is_public
-  repository_type  = "DOCKER"
+  compartment_id = var.compartment_ocid
+  display_name   = "oci-function-python-customimage-${random_id.tag.hex}"
+  is_public      = var.container_repository_is_public
 }
