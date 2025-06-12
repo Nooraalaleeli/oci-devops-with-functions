d3804bf9eac1fdc66129021ff3da48081eb467c7
Nooraalaleeli <nooramusabeh1@gmail.com>
2025-06-13 00:06:34 +0400

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -7,34 +7,39 @@ terraform {
   required_providers {
     oci = {
       source = "hashicorp/oci"
+      # version = "4.34.0"
     }
     tls = {
       source  = "hashicorp/tls"
+      version = "2.0.1" # Latest version as March 2021 = 3.1.0. Using 2.0.1 (April, 2020) for ORM compatibility
     }
     local = {
       source  = "hashicorp/local"
+      version = "1.4.0" # Latest version as March 2021 = 2.1.0. Using 1.4.0 (September, 2019) for ORM compatibility
     }
     random = {
       source  = "hashicorp/random"
+      version = "2.3.0" # Latest version as March 2021 = 3.1.0. Using 2.3.0 (July, 2020) for ORM compatibility
     }
   }
 }
 
 provider "oci" {
-  alias            = "home_region"
-  region           = var.home_region
-  tenancy_ocid     = var.tenancy_ocid
-  user_ocid        = var.user_ocid
-  fingerprint      = var.fingerprint
-  private_key_path = var.private_key_path
+  tenancy_ocid = var.tenancy_ocid
+  region       = var.region
+
 }
 
 provider "oci" {
-  alias            = "current_region"
-  region           = var.region
-  tenancy_ocid     = var.tenancy_ocid
-  user_ocid        = var.user_ocid
-  fingerprint      = var.fingerprint
-  private_key_path = var.private_key_path
+  alias        = "home_region"
+  tenancy_ocid = var.tenancy_ocid
+  region       = lookup(data.oci_identity_regions.home_region.regions[0], "name")
+
 }
 
+provider "oci" {
+  alias        = "current_region"
+  tenancy_ocid = var.tenancy_ocid
+  region       = var.region
+
+}
